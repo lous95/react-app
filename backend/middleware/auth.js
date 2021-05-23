93340635fd6c92722aadfefedaf174243181b5e2
lous95 <louays95@gmail.com>
2021-05-23 23:12:38 +0300

    update to jwt token in middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtKey'));
+    const decoded = jwt.verify(token, "YourPrivateKey");
     req.user = decoded;
     next();
   }
