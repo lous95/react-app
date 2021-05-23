54ad5ed8d5c681c67e94ca7687abe2eecd19198f
lous95 <louays95@gmail.com>
2021-05-23 17:51:57 +0300

    changes

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use('/api/events', events);
 
 const port = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('../frontend/build'));
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('../frontend/build'));
+// }
 http.listen(port, () => console.log(`Listening on port ${port}...`));
